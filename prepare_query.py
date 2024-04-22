e115472ec5f3c781786df45925f807fd6a86ee18
Meet1903 <meet.diwan.md@gmail.com>
2024-04-21 20:18:46 -0400

    Basic query preparer for searching

---
+++ prepare_query.py
@@ -0,0 +1,23 @@
+import re
+
+unnecessary_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 
+              'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 
+              'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 
+              'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 
+              'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 
+              'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before',
+              'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again',
+              'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few',
+              'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very',
+              'can', 'will', 'just', 'don', 'should', 'now']
+
+def remove_unnecessary_words(query):
+    query = [word for word in query.split() if word not in unnecessary_words]
+    return ' '.join(query)
+
+def prepare_query(query):
+    query = query.lower()
+    query = re.sub(r'[^a-zA-Z0-9]+', ' ', query)
+    query = query.strip()
+    query = remove_unnecessary_words(query)
+    return query
