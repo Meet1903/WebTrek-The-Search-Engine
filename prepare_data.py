71b747c7efde3d0dea1e38979f6201124076afa9
Meet1903 <meet.diwan.md@gmail.com>
2024-04-23 20:36:28 -0400

    Resolved issue with id size limit, handled exception

--- prepare_data.py
+++ prepare_data.py
@@ -3,42 +3,56 @@ from bs4 import BeautifulSoup
 import os
 import re
 from urllib.parse import urlparse
+import hashlib
 
+def generate_short_id(url):
+    hash_object = hashlib.md5(url.encode())
+    return hash_object.hexdigest()
+
+def is_string_larger_tan_512(string, encoding='utf-8'):
+    encoded_bytes = string.encode(encoding)
+    return len(encoded_bytes) > 512
 
 def extract_text_from_html_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        html = file.read()
-        soup = BeautifulSoup(html, 'html.parser')
-        if soup.find('meta', {'property': 'og:url'}):
-            url = soup.find('meta', {'property': 'og:url'})['content']
-        else:
-            return
-        # Extract the domain from the URL
-        domain = urlparse(url).netloc
-        if soup.find('h1'):
-            title = soup.find('h1').get_text().strip()
-        else:
-            title = url
-        text = soup.get_text()
-        text = re.sub(r'[^a-zA-Z0-9]+', ' ', text)
-        text = text.strip()
-        lines = text.split('\n')
-        # text = '|'.join(lines)  # Separate lines with '|'
-        lines_without_empty = [line.strip() for line in lines if line.strip()]
-        text = '    '.join(lines_without_empty)
-
-        document = {
-            "_index": "websearch",
-            "_id": url,
-            "_source": {
-                "title": title,
-                "domain": domain,
-                "url": url,
-                "content": text,
-            },
-        }
-        return document
-    return
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            html = file.read()
+            soup = BeautifulSoup(html, 'html.parser')
+            if soup.find('meta', {'property': 'og:url'}):
+                url = soup.find('meta', {'property': 'og:url'})['content']
+            else:
+                return
+            # Extract the domain from the URL
+            domain = urlparse(url).netloc
+            if soup.find('h1'):
+                title = soup.find('h1').get_text().strip()
+            else:
+                title = url
+            text = soup.get_text()
+            text = re.sub(r'[^a-zA-Z0-9]+', ' ', text)
+            text = text.strip()
+            lines = text.split('\n')
+            # text = '|'.join(lines)  # Separate lines with '|'
+            lines_without_empty = [line.strip() for line in lines if line.strip()]
+            text = '    '.join(lines_without_empty)
+            if is_string_larger_tan_512(url):
+                id = generate_short_id(url)
+            else:
+                id = url
+            document = {
+                "_index": "newsearch",
+                "_id": id,
+                "_source": {
+                    "title": title,
+                    "domain": domain,
+                    "url": url,
+                    "content": text,
+                },
+            }
+            return document
+    except Exception as e:
+        print("Error in data loading", e)
+        return
 
 def read_html_files(folder_path):
     bulk_data = []
